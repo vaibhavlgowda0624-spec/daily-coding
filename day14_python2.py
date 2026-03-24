bc24f0b3cdef9a7dad9b77f683fe2086332b6ce8
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-03-24 18:28:49 +0530

    Check Prime Number

---
+++ day14_python2.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+
+        if num % i == 0:
+            print("Not a Prime Number")
+            break
+
+   else:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
