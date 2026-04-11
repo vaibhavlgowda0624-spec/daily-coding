8f99267a6c4b8d0002dd751df604a842da9e60d5
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-04-11 20:38:25 +0530

    Simple calculator function

---
+++ day31_python1.py
@@ -0,0 +1,12 @@
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mul(a, b): return a * b
+def div(a,b): return a / b
+
+a = int(input("Enter first: "))
+b = int(input("Enter second: "))
+
+print("Add:", add(a,b))
+print("Sub:", sub(a,b))
+print("Mul:", mul(a,b))
+print("Div:", div(a,b))
