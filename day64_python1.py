4170a5c8357ab9113aae53d909839348aeb69acf
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-05-12 22:11:02 +0530

    Implement insertion sort algorithm in Python

---
+++ day64_python1.py
@@ -0,0 +1,13 @@
+nums = [5,3,1,4]
+
+for i in range(1,len(nums)):
+    key = nums[i]
+    j = i-1
+    
+    while j >=0 and nums[j] > key:
+        nums[j+1] = nums[j]
+        j -= 1
+    
+    nums[j+1] = key
+
+print(nums)
