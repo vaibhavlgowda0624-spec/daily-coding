00b0f99dc38efac1a7e401aba19987d8ccc1132c
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-05-15 22:10:43 +0530

    Add simple login authentication script

---
+++ day67_python2.py
@@ -0,0 +1,10 @@
+username = "admin"
+password = "1234"
+
+u = input("Username: ")
+p = input("Password: ")
+
+if u == username and p == password:
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
