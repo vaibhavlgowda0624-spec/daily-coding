80a7a4f5e4787ff057a18c70e79eae05c7b7b761
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-06-21 22:16:48 +0530

    Implement binary search function in Python

---
+++ day104_python1.py
@@ -0,0 +1,17 @@
+def binary_search(arr,x):
+    low=0
+    high=len(arr)-1
+
+    while low<=high:
+        mid=(low+high)//2
+
+        if arr[mid]==x:
+            return mid
+        elif arr[mid]<x:
+            low=mid+1
+        else:
+            high=mid-1
+
+    return -1
+
+print(binary_search([1,2,3,4,5],4))
