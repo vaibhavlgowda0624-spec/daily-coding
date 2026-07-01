b404c08f69b5bcaec80529f547ce37e85f2ebaae
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-07-01 22:09:07 +0530

    Implement bubble sort algorithm in Python

---
+++ day114_python1.py
@@ -0,0 +1,9 @@
+def bubble(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+print(bubble([7,3,9,2]))
