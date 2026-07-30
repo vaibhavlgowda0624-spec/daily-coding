51f222f63efbfb701579525a3fbdfac9492142b0
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-07-30 23:06:57 +0530

    Create students database and table in SQLite

---
+++ day141_python2.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect("students.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS students(
+id INTEGER PRIMARY KEY,
+name TEXT,
+marks INTEGER
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Database Created")
