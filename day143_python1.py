83c27bca61651fbc6189734c575b38dfcdbcbbcf
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-08-01 23:05:10 +0530

    Add script to fetch and print student records

---
+++ day143_python1.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conn = sqlite3.connect("students.db")
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM students")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
