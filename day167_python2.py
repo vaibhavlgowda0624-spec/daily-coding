3e6d8a29f6a79ef83f9eb2fed209ec9737f2564f
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-08-25 23:00:20 +0530

    Implement bubble sort algor

---
+++ day167_python2.py
@@ -0,0 +1,11 @@
+numbers = [64, 34, 25, 12, 22, 11, 90]
+
+n = len(numbers)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+print(numbers)
