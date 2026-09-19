be4b35534a89ab170e8ae441a9dc3e7b51b3aa0b
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-09-19 23:42:12 +0530

    Implement basic stack operations in Python

---
+++ day185_python1.py
@@ -0,0 +1,12 @@
+stack = []
+
+stack.append("Book")
+stack.append("Pen")
+stack.append("Laptop")
+
+print("Stack:", stack)
+
+removed = stack.pop()
+
+print("Removed:", removed)
+print("Updated Stack:", stack)
