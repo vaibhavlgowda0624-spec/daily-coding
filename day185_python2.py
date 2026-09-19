e9bbb7fcf420ef91f3a3cb1e1f256c9eb8866cf0
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-09-19 23:42:41 +0530

    Implement basic queue operations in Python

---
+++ day185_python2.py
@@ -0,0 +1,12 @@
+queue = []
+
+queue.append("Person 1")
+queue.append("Person 2")
+queue.append("Person 3")
+
+print("Queue:", queue)
+
+served = queue.pop(0)
+
+print("Served:", served)
+print("Remaining Queue:", queue)
