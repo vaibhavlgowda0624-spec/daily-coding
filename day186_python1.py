6f906c586adb9ac6181bf84b9eec460e6019e4da
Vaibhav L Gowda <vaibhavlgowda0624@gmail.com>
2026-09-20 23:44:16 +0530

    Implement factorial function in Python

---
+++ day186_python1.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+number = int(input("Enter a number: "))
+
+print("Factorial:", factorial(number))
